efa56fe002e0a4ee54b1557ba2863a4c9c3b0689
UnknownNull <parsgameplayir@gmail.com>
2023-10-04 18:58:48 +0330

    update to pm5 and php 8.2

--- src/UnknownNull/BitcoinAPI/task/SortTask.php
+++ src/UnknownNull/BitcoinAPI/task/SortTask.php
@@ -1,22 +1,6 @@
 <?php
 
-/*
- * PointS, the massive point plugin with many features for PocketMine-MP
- * Copyright (C) 2013-2017  UnknownNull <jyc00410@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+ 
 
 namespace UnknownNull\BitcoinAPI\task;
 
@@ -32,14 +16,15 @@ class SortTask extends AsyncTask{
     private $max = 0;
 
     private $topList;
+    private $coinData;
 
     /**
-     * @param string            $player
-     * @param array                $pointData
-     * @param bool                $addOp
-     * @param int                $page
-     * @param array                $ops
-     * @param array                $banList
+     * @param string $sender
+     * @param array $BitcoinData
+     * @param bool $addOp
+     * @param int $page
+     * @param array $ops
+     * @param array $banList
      */
     public function __construct(string $sender, array $BitcoinData, bool $addOp, int $page, array $ops, array $banList){
         $this->sender = $sender;
